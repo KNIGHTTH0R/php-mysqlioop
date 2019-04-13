250a03e7252ee1f48bde0b10a5324368cfdf622b
mukhoiran <mkhoir.anam@gmail.com>
2019-04-13 23:49:36 +0700

    prepare statement

--- index.php
+++ index.php
@@ -13,15 +13,31 @@ if($mysqli->connect_errno){
   echo 'Failed connect to mysql'.$mysqli->connect_error;
 }
 
-$sql = "INSERT INTO student(name,address) VALUES ('bara','bekasi');";
-$sql .= "INSERT INTO student(name,address) VALUES ('anval','semarang')";
+// $sql = "INSERT INTO student(name,address) VALUES ('bara','bekasi');";
+// $sql .= "INSERT INTO student(name,address) VALUES ('anval','semarang')";
 
 //execute single query
 // if($mysqli->query($sql)) echo 'success';
 
 //execute multiple query
-if($mysqli->multi_query($sql)) echo 'success';
-else echo 'failed'.$mysqli->error;
+// if($mysqli->multi_query($sql)) echo 'success';
+// else echo 'failed'.$mysqli->error;
+
+//sql injection
+// $name = $mysqli->real_escape_string($_GET['user']);
+
+//prepare statement -> no need escape string
+$statement = $mysqli->prepare('INSERT INTO student (name, address) VALUES (?,?)');
+$statement->bind_param('ss', $name, $address);
+
+//input parameter and execute
+$name = 'bani';
+$address = 'karawang';
+$statement->execute();
+
+$name = 'boriel';
+$address = 'bandung';
+$statement->execute();
 
 $mysqli->close();
 
