b65d2fe6c94479d1f8f13818689b641beb644ef4
mukhoiran <mkhoir.anam@gmail.com>
2019-04-13 23:37:48 +0700

    insert data

--- index.php
+++ index.php
@@ -5,10 +5,19 @@ $user = 'root';
 $pass = '';
 $db = 'php-mysqlioop';
 
+//connect db
 $mysqli = new mysqli($host, $user, $pass, $db);
 
 //test connection db
 if($mysqli->connect_errno){
   echo 'Failed connect to mysql'.$mysqli->connect_error;
 }
+
+$sql = "INSERT INTO student(name,address) VALUES ('khoirul','jakarta')";
+
+if($mysqli->query($sql)) echo 'success';
+else echo 'failed'.$mysqli->error;
+
+$mysqli->close();
+
 ?>
