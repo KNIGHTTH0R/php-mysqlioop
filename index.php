6c380615c96f0194183f2075546e605556ba13da
mukhoiran <mkhoir.anam@gmail.com>
2019-04-13 23:40:28 +0700

    insert multiple row

--- index.php
+++ index.php
@@ -13,9 +13,14 @@ if($mysqli->connect_errno){
   echo 'Failed connect to mysql'.$mysqli->connect_error;
 }
 
-$sql = "INSERT INTO student(name,address) VALUES ('khoirul','jakarta')";
+$sql = "INSERT INTO student(name,address) VALUES ('bara','bekasi');";
+$sql .= "INSERT INTO student(name,address) VALUES ('anval','semarang')";
 
-if($mysqli->query($sql)) echo 'success';
+//execute single query
+// if($mysqli->query($sql)) echo 'success';
+
+//execute multiple query
+if($mysqli->multi_query($sql)) echo 'success';
 else echo 'failed'.$mysqli->error;
 
 $mysqli->close();
